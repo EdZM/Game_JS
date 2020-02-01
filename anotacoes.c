83902ddfffef700a5daf3e035d452dd46f955c5a
Eduardo Mirolli <eduardomirolli7@gmail.com>
2020-01-31 21:04:26 -0300

    Add files via upload

---
+++ anotacoes.c
@@ -0,0 +1,22 @@
+/*
+    Jogo multiplayer feito em Java Script 
+
+    - A principio temos 4 camadas:
+        - Apresentação: apresenta resultado final da aplicação
+        - Lógica e Dados: guarda as informações/estado(Dados) e as regras do jogo(Lógica) ==> State Machine + logica
+        - Input: reage aos comandos do usuário executando uma ação. Deve enviar dados para a Camada de logica+dados, pois só ela tem as informações necessárias,
+                 para decidir o que será feito e, posteriormente, apresentado.
+        - Networking: responsável por lidar com a parte do multiplayer, onde existem vários clients ligados sincronizadamente a um mesmo server
+
+
+
+    - Gitpod: IDE online para github em que posso deixar um ambiente de desenvolvimento totalmente configurado para que qualquer pessoa possa usar
+
+
+
+
+
+
+
+
+*/
