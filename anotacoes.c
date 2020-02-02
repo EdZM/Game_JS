6d3ce61e07d35f77706e2b80173a772a8b1815a8
Eduardo Mirolli <eduardomirolli7@gmail.com>
2020-02-02 13:34:44 +0000

    novas anotacoes

--- anotacoes.c
+++ anotacoes.c
@@ -23,11 +23,15 @@
 
     - é necessário estar atento para que uma camada não tenha responsabilidades de outras ==> Isso dificulta uma eventual manutenção, já que se eu tiver um erro na camada de 
         apresentação, ele poderá, por exemplo, estar relacionado com o funcionamento de outra camada que não tem nada a ver com essa.
+        Isso também dificulta eventuais testes, envio de informações para o backend, ...
+        Portanto, sempre pense em modelagem, arquitetura de software na hora de codar um projeto, para que ele não fique um código ameba.
+
+    - na implementação do jogo há o conceito de acoplamento.    
 
 
-    - a cada linha escrita sempre questione: ""
-
-
+    - a cada linha escrita sempre questione: "de quem é esse código?" .Isso tem o intuito de me fazer perceber as divisórias do meu sistema ou os limites dos componentes e se um está acoplado ao outro ou quando isso acontece
+        - Se a resposta a essa pergunta for sempre que o código/linha pertence sempre a uma mesma camada, estou gerando um código ameba
+        - Caso eu não saiba a resposta a essa pergunta, então não tenho total compreensão da modelagem do meu código.
 
 
 */
