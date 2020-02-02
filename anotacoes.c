ce0828de484e3957f72eab6f95684cfae4cad498
Eduardo Mirolli <eduardomirolli7@gmail.com>
2020-02-02 13:19:50 +0000

    novas anotacoes

--- anotacoes.c
+++ anotacoes.c
@@ -12,11 +12,20 @@
 
     - Gitpod: IDE online para github em que posso deixar um ambiente de desenvolvimento totalmente configurado para que qualquer pessoa possa usar
 
-    - Uso do módulo serve: 
+    - Uso do módulo 'serve': 
             - por meio dele consido rodar o .html que está no container do gitpod
             - transforma a pasta que em estou num servidor estático
             - para usá-lo: npx serve
 
+    - Design Pattern usado:
+
+    - Primeira regra de negócio do jogo: estabelecer regras para que o jogador não saia dos limites do canvas
+
+    - é necessário estar atento para que uma camada não tenha responsabilidades de outras ==> Isso dificulta uma eventual manutenção, já que se eu tiver um erro na camada de 
+        apresentação, ele poderá, por exemplo, estar relacionado com o funcionamento de outra camada que não tem nada a ver com essa.
+
+
+    - a cada linha escrita sempre questione: ""
 
 
 
